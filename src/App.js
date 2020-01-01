5c1549e9f0f58c502e3c7c84ba4cccda645d1974
Szobonya Gergő <djoke23@freemail.hu>
2020-01-01 21:55:05 +0100

    React default file added.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Router from './Router';
+import {Redirect} from 'react-router-dom';
+
+function App() { 
+  return (
+  	<div>
+  	<Router  />
+  		
+  	</div>
+
+    );
+}
+
+export default App;
